b25a9476ad9ebc43486b802d0f82464dc3eccd91
Yudan Pan <pydloui@gmail.com>
2025-12-24 21:16:20 +0100

    enemy platform changed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,31 +1,64 @@
 using UnityEngine;
-
+using System.Collections;
 public class Enemy : MonoBehaviour
 {
   public float moveSpeed = 2f;
-  private Vector2 direction;
+  public Transform groundCheckPoint;
+  public Transform wallCheckPoint;
+  public float detectionDistance = 0.6f;
 
-  void Start()
+  float lastFlipTime;
+  float flipCooldown = 0.2f; // 翻转冷却时间
+  public LayerMask groundLayer;
+
+  int direction = 1;
+  Rigidbody2D rb;
+  bool canDetect;
+
+  IEnumerator Start()
   {
-    direction = Random.insideUnitCircle.normalized;
+    rb = GetComponent<Rigidbody2D>();
+    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+    yield return null; // 等一帧
+    canDetect = true;
   }
 
-  void Update()
+  void FixedUpdate()
   {
-    transform.Translate(direction * moveSpeed * Time.deltaTime);
+    rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+
+    if (!canDetect) return;
+
+    bool hasGround = Physics2D.Raycast(
+        groundCheckPoint.position,
+        Vector2.down,
+        detectionDistance,
+        groundLayer
+    );
+
+    bool hitWall = Physics2D.Raycast(
+        wallCheckPoint.position,
+        Vector2.right * direction,
+        0.2f,
+        groundLayer
+    );
+
+    if (!hasGround || hitWall)
+    {
+      Flip();
+    }
   }
 
-  private void OnCollisionEnter2D(Collision2D collision)
+  void Flip()
   {
-    if (collision.gameObject.CompareTag("Player"))
-    {
-      Debug.Log("Game Over");
-      Time.timeScale = 0f;
-    }
+    if (Time.time < lastFlipTime + flipCooldown) return; // 冷却中不执行翻转
 
-    if (collision.gameObject.CompareTag("Ground"))
-    {
-      direction = -direction;
-    }
+    direction *= -1;
+    Vector3 s = transform.localScale;
+    s.x *= -1;
+    transform.localScale = s;
+
+    lastFlipTime = Time.time;
   }
 }
