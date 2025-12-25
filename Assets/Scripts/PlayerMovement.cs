3c81ba54d4d7b4f3c7764b819ab2401b516f3764
VeronikaZhaboklitskaya <veronika.zhaboklitskaya@gmail.com>
2025-12-25 22:11:47 +0100

    Revert "level 1 finished"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  [Header("Movement Settings")]
-  public float moveSpeed = 8f;
-  public float jumpForce = 15f;
-  public float bounceForce = 12f;
-
-  [Header("Detection")]
-  public Transform groundCheck;
-  public float checkRadius = 0.4f;
-  public LayerMask groundLayer;
+  [SerializeField] private float moveSpeed = 8f;
+  [SerializeField] private float jumpForce = 15f;
 
   private Rigidbody2D rb;
   private bool isGrounded;
-  private float moveInput;
-  private bool jumpPressed;
-
-  private Rigidbody2D currentPlatformRb;
-  private Vector2 lastPlatformPos;
-  private Vector2 platformVelocity;
-  private bool onPlatform;
 
   private float xPosLastFrame;
     
@@ -30,41 +16,17 @@ public class PlayerMovement : MonoBehaviour
     void Start()
   {
     rb = GetComponent<Rigidbody2D>();
-    rb.interpolation = RigidbodyInterpolation2D.Interpolate;
-    rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-    rb.gravityScale = 4f;
   }
 
   void Update()
   {
-    moveInput = Input.GetAxis("Horizontal");
-    if (Input.GetKeyDown(KeyCode.Space)) jumpPressed = true;
-
-    isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+    Move();
+    Jump();
+    FlipCharacterX();
   }
 
-  void FixedUpdate()
+  void Move()
   {
-    if (onPlatform && currentPlatformRb != null)
-    {
-      platformVelocity = (currentPlatformRb.position - lastPlatformPos) / Time.fixedDeltaTime;
-      lastPlatformPos = currentPlatformRb.position;
-    }
-    else
-    {
-      platformVelocity = Vector2.zero;
-    }
-
-    Move();
-
-    if (jumpPressed)
-    {
-      if (isGrounded)
-      {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-      }
-      jumpPressed = false;
-    }
     float moveInput = Input.GetAxis("Horizontal");
     rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
@@ -81,57 +43,47 @@ public class PlayerMovement : MonoBehaviour
         
   }
 
-  void Move()
+  void Jump()
   {
-    float targetX = (moveInput * moveSpeed) + platformVelocity.x;
-    float targetY = rb.linearVelocity.y;
-
-    if (onPlatform && !jumpPressed && rb.linearVelocity.y <= 0.1f)
+    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
     {
-      targetY = platformVelocity.y - 0.05f;
-    }
-
-    rb.linearVelocity = new Vector2(targetX, targetY);
-  }
-
-  private void OnCollisionStay2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("MovingStone"))
-    {
-      if (collision.contacts[0].normal.y > 0.5f)
-      {
-        currentPlatformRb = collision.rigidbody;
-        if (!onPlatform)
-        {
-          lastPlatformPos = currentPlatformRb.position;
-          onPlatform = true;
-        }
-      }
-    }
-  }
-
-  private void OnCollisionExit2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("MovingStone"))
-    {
-      onPlatform = false;
-      currentPlatformRb = null;
+      rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
   }
 
   private void OnCollisionEnter2D(Collision2D collision)
   {
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+      isGrounded = true;
+    }
     if (collision.gameObject.CompareTag("Enemy"))
     {
-      if (collision.contacts[0].normal.y > 0.5f)
-      {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceForce);
-        Destroy(collision.gameObject);
-      }
-      else
-      {
-        GameManager.instance.TakeDamage(1);
-      }
+      Debug.Log("Game Over");
+      Time.timeScale = 0f;
     }
   }
+
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+      isGrounded = false;
+    }
+  }
+
+    private void FlipCharacterX()
+    {
+        if (transform.position.x > xPosLastFrame)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (transform.position.x < xPosLastFrame)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+        xPosLastFrame = transform.position.x;
+
+    }
 }
