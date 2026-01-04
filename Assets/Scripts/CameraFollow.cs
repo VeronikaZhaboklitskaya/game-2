689968de0e8bf7790b0a5715696f9ae733d5adf2
VeronikaZhaboklitskaya <veronika.zhaboklitskaya@gmail.com>
2026-01-04 16:41:54 +0100

    CameraFollow Script
    Smoother camera follow movement (can be later adjusted)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    public float FollowSpeed = 1.5f;
+    public float yOffset = 1f;
     public Transform target;
-    public Vector3 offset;
-  
-    void Update ()
+
+    void Update()
     {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);    
     }
 }
