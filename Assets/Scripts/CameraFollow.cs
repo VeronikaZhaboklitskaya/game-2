2d0312c1bf0013d613e8e3fd19f671a4f3387d63
VeronikaZhaboklitskaya <veronika.zhaboklitskaya@gmail.com>
2026-01-11 23:05:47 +0100

    Couple of textures added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,12 @@ public class CameraFollow : MonoBehaviour {
 
     public float FollowSpeed = 1.5f;
     public float yOffset = 1f;
-    public float xOffset = 3f;
+    public float xOffset = 3.5f;
     public Transform target;
 
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -50f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);    
     }
 }
